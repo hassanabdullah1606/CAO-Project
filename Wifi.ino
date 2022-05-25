37e6ec42150f18199ff64a0154176ee36aa43ca2
Hassan Abdullah <96684499+hassanabdullah1606@users.noreply.github.com>
2022-05-26 00:18:23 +0500

    Add files via upload

---
+++ Wifi.ino
@@ -0,0 +1,48 @@
+#include "WiFi.h"
+const char* ssid = "Galaxy S20 FEF378";
+const char* password = "111222333";
+
+unsigned long previousMillis = 0;
+unsigned long interval = 10000;
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+
+  
+  unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
+  }
+
+  
+}
+
+
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
